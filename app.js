1a34a15c7abfa5ea434588129b3364039657091f
ceciliaoneil <ceciliaoneil.di@gmail.com>
2022-09-23 19:56:24 +0000

    dominios

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+
+
+
+for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+        for (let z = 0; z < noun.length; z++) {
+            console.log(pronoun[x] + adj[y] + noun[z] + ".com");
+        }
+    }
+} 
+
